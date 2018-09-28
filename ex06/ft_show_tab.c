385307b3b7c76e9deb3539318aa12b984e547188
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-28 02:59:02 -0700

    add one element per line

--- ex06/ft_show_tab.c
+++ ex06/ft_show_tab.c
@@ -6,7 +6,7 @@
 /*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/28 01:33:14 by seli              #+#    #+#             */
-/*   Updated: 2018/09/28 02:57:03 by seli             ###   ########.fr       */
+/*   Updated: 2018/09/28 02:58:53 by seli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,9 @@ void	ft_show_tab(struct s_stock_par *par)
 	{
 		word = 0;
 		ft_putstr(par[i].copy);
+		ft_putchar('\n');
 		ft_putnbr(par[i].size_param);
+		ft_putchar('\n');
 		while (par[i].tab[word])
 		{
 			ft_putstr(par[i].tab[word++]);
