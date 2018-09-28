84afe06b35b7b425c19e8c9de5480ffc7899b6c3
Sen Li <seli@e1z1r7p16.42.us.org>
2018-09-27 23:17:13 -0700

    norm ft.h

---
+++ ex01/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seli <seli@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/27 23:10:13 by seli              #+#    #+#             */
+/*   Updated: 2018/09/27 23:13:08 by seli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int		ft_strlen(char *str);
+void	ft_swap(int *a, int *b);
+#endif
